10788e763451ebbd5b27fb0da8fea47a670e55c6
ricardoMogas <al066208@uacam.mx>
2024-03-11 13:24:00 -0600

    Corrección, import queryjs y minjs de bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
