6737df1542dfe27936757015a4482715cc8acc2d
Gael Alexander Carrillo Chan <al068322@uacam.mx>
2024-03-23 18:02:00 -0600

    Modificacion de HomePage con Bootstrap

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,76 +9,60 @@ const HomePage = () => {
   };
 
   return (
-    <>
+    <div>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>Pagina de bienvenida</title>
-      <style dangerouslySetInnerHTML={{ __html: "\n button {\n padding: 10px 20px; * ajusta el tamaño del botón \n font-size: 15px; texto font-family: 'gill sans', sans mt', calibri, 'trebuchet ms', sans-serif;\n background-color: lightblue;\n }\n " }} />
-      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <img src="PFILogo.png" alt="Foto" style={{ width: "70%", maxWidth: 80, alignContent: "center" }} />
-          <div>
-            <button>Control de Alumnos</button>
-            <button>Registro de visitas</button>
-            <button>Servicios</button>
-            <button>Reportes</button>
+      <title>Pagina de bienvenida PFI</title>
+      {/* Incluir Bootstrap CSS */}
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+      <div className="container-fluid h-100 d-flex flex-column mt-3"> 
+        <div className="row align-items-start text-left">  {/* Cambiado a 'align-items-start' para alinear al inicio */}
+          <div className="col">
+            <img src="iconpfi.png" alt="Foto" className="img-fluid" style={{maxWidth: '90px'}} /> {/* Añadido estilo para controlar el tamaño */}
+            <div className="btn-group" role="group">
+              <button type="button" className="btn btn-primary">Control de Alumnos</button>
+              <button type="button" className="btn btn-primary">Registro de visitas</button>
+              <button type="button" className="btn btn-primary">Servicios</button>
+              <button type="button" className="btn btn-primary">Reportes</button>
+            </div>
           </div>
         </div>
-        <button>
-          <img src="TuercaSF.png" alt="Imagen del botón 5" style={{ width: "100%", maxWidth: 30, padding: 0, alignContent: "center" }} />
-        </button>
-      </div>
-      <div style={{ position: "absolute", top: "30%", left: "5%" }}>
-        <img src="PFILogo.png" alt="Foto" style={{ width: "100%", maxWidth: 400 }} />
-      </div>
-      <div style={{ position: "absolute", top: "40%", left: "30%", right: "20%" }}>
-        {/* Scrollbar para el texto justo a la derecha */}
-        <div style={{ overflowY: "scroll", height: 200, right: "60%" }}>
-          <p>
-            Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si
-            Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe
-            Pecas pica papas? Pepe Pecas pica papas con un pico, con un pico pica
-            papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el
-            pico con que Pepe Pecas pica papas? Pepe Pecas pica papas con un pico,
-            con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico,
-            ¿dónde está el pico con que Pepe Pecas pica papas? Pepe Pecas pica papas
-            con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas
-            con un pico, ¿dónde está el pico con que Pepe Pecas pica papas? Pepe
-            Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe
-            Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas
-            pica papas? Pepe Pecas pica papas con un pico, con un pico pica papas
-            Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico
-            con que Pepe Pecas pica papas? Pepe Pecas pica papas con un pico, con un
-            pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde
-            está el pico con que Pepe Pecas pica papas? Pepe Pecas pica papas con un
-            pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un
-            pico, ¿dónde está el pico con que Pepe Pecas pica papas? Pepe Pecas pica
-            papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica
-            papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
-            Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si
-            Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe
-            Pecas pica papas? Pepe Pecas pica papas con un pico, con un pico pica
-            papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el
-            pico con que Pepe Pecas pica papas? Pepe Pecas pica papas con un pico,
-            con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico,
-            ¿dónde está el pico con que Pepe Pecas pica papas? Pepe Pecas pica papas
-            con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas
-            con un pico, ¿dónde está el pico con que Pepe Pecas pica papas? Pepe
-            Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe
-            Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas
-            pica papas? Pepe Pecas pica papas con un pico, con un pico pica papas
-            Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico
-            con que Pepe Pecas pica papas? Pepe Pecas pica papas con un pico, con un
-            pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde
-            está el pico con que Pepe Pecas pica papas? Pepe Pecas pica papas con un
-            pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un
-            pico, ¿dónde está el pico con que Pepe Pecas pica papas? Pepe Pecas pica
-            papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica
-            papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
-          </p>
+        <div className="row align-items-cen mt-3 flex-grow-1"> {/* Añadido 'flex-grow-1' para hacer que esta fila ocupe todo el espacio restante */}
+          <div className="col-12 col-md-6">
+            <img src="iconpfi.png" alt="Foto" className="img-fluid" style={{maxWidth: '500px'}} />
+          </div>
+          <div className="col-12 col-md-6"> {/* Añadido 'd-flex' para hacer que el contenido de esta columna se estire para llenar el espacio vertical */}
+            <div className="card w-100"> {/* Añadido 'flex-grow-1' para hacer que la tarjeta ocupe todo el espacio vertical y 'w-100' para llenar el espacio horizontal */}
+              <div className="card-body" style={{height: '400px', overflow: 'auto'}}>
+                <p className="card-text">
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pablitoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pablitoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                  Pepe Pecas pica papas con un pico, con un pico pica papas Pepe Pecas. Si Pepe Pecas pica papas con un pico, ¿dónde está el pico con que Pepe Pecas pica papas?
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row mt-4">
+          <div className="col text-center">
+            <p>¡Gracias por visitar el PFI! Esperamos que disfrutes de tu estancia.</p>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
