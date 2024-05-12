e5eeaca31b3b08352a6f442c0bba7f27fc1034f8
ricardoMogas <al066208@uacam.mx>
2024-05-11 22:54:00 -0600

    Corrección de consumo de copies

--- src/store/CopiasFetch.js
+++ src/store/CopiasFetch.js
@@ -3,48 +3,41 @@ import axios from 'axios';
 class CopiasFetcher {
 
     constructor(API_BASE_URL) {
-        this.API_BASE_URL = API_BASE_URL;
+      this.API_BASE_URL = API_BASE_URL;
     }
 
-  async GetCopias() {
-    try {
-      const response = await axios.get(this.API_BASE_URL + '/copias-e-impresiones');
-      const copias = response.data.result.map(copias => ({ ...copia, checked: false }));
-      return copias;
-    } catch (error) {
-      console.error('Error fetching copias e impresiones:', error);
+    async GetTotalCopies(registration, date) {
+      try {
+        const response = await axios.get(`${this.API_BASE_URL}/Copies?registration=${registration}&date=${date}`);
+        const resultResponse = response.data.result;
+        return resultResponse;
+      } catch (error) {
+        console.error('Error fetching copias:', error);
+      }
     }
-  }
 
-  async GetCopiasPagination(page, perPage, filters) {
-    try {
-      const response = await axios.get(`${this.API_BASE_URL}/Copias?page=${page}&perPage=${perPage}`, {
-        params: filters
-      });
-      const resultResponse = response.data.result;
-      const copias = resultResponse.data.map(copia => ({ ...copia, checked: false }));
-      return copias;
-    } catch (error) {
-      console.error('Error fetching copias:', error);
+    async RegisterCopia(registration, totalCopies, date) {
+      try {
+        const result= await axios.post(`${this.API_BASE_URL}/Copies?registration=${registration}&total=${totalCopies}&date=${date}`);
+        return result.data;
+      } catch (error) {
+        console.error('Error registering copia:', error);
+      }
     }
-  }
-
-  async RegisterCopia(copiaData) {
-    try {
-      const response = await axios.post(`${this.API_BASE_URL}/copias-e-impresiones`, {
-        params: copiaData,
-      });
-      return result;
-    } catch (error) {
-      console.error('Error registering copia:', error);
-    }
-  }
 }
 export default CopiasFetcher;
+// register copies example
+/*
+const copiasfetcher = new CopiasFetcher("http://localhost/PFI-Services-Api");
+const date = new Date().toISOString().split('T')[0];
 
-// USO DE LA CLASE CopiasFetcher
-/*
-const CopiasFetcher = new CopiasFetcher("http://localhost/PFI-Services-Api");
-const copias = await CopiasFetcher.GetCopias();
-console.log(copias);
-/*
+
+const NewCopies = new CopiasFetcher("http://localhost/PFI-Services-Api");
+const result = await NewCopies.RegisterCopia(66208, 1, date);
+console.log(result);
+
+
+const copiasTotal = await copiasfetcher.GetTotalCopies(66208, date);
+console.log(copiasTotal);
+console.log(date);
+*/
